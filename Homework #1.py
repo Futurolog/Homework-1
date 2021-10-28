15001eea92a139b7f8c6b3dde57b27d7410e3063
Futurolog <92756364+Futurolog@users.noreply.github.com>
2021-10-28 06:19:29 +0500

    Update and rename Homework #1 to Homework #1.py

---
+++ Homework #1.py
@@ -0,0 +1,68 @@
+Задача №1
+
+num1 = 4
+num2 = 8
+print(num1, num2)
+number1 = ('Введите число')
+number2 = ('Введите число')
+string1 = ('Введите строку')
+string2 = ('Введите строку')
+print(number1, number2, string1, string2)
+
+Задача №2
+
+time = (input("Введите время в секундах "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print(format"hours:minutes:seconds")
+
+Задача №3
+
+n = int(input("Введите число - "))
+total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+print("Сумма чисел n + nn + nnn - %d" % total)
+
+Задача №4
+
+n = abs(int(input("Введите целое положительное число ")))
+max = n % 10
+while n >= 1:
+    n = n // 10
+    if n % 10 > max:
+        max = n % 10
+    if n > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max)
+        break
+        
+Задача №5
+
+profit = float(input("Введите выручку фирмы "))
+costs = float(input("Введите издержки фирмы "))
+if profit > costs:
+    print(f"Фирма работает с прибылью. Рентабельность выручки составила {profit / costs:.2f}")
+    workers = int(input("Введите количество сотрудников фирмы "))
+    print(f"прибыль в расчете на одного сторудника сотавила {profit / workers:.2f}")
+elif profit == costs:
+    print("Фирма работает в ноль")
+else:
+    print("Фирма работает в убыток")
+    
+Задача №6
+
+a = int(input("Введите результаты пробежки первого дня в км "))
+b = int(input("Введите общий желаемый результат в км "))
+result_days = 1
+result_km = a
+while result_km < b:
+        a = a + 0.1 * a
+        result_days += 1
+        result_km = result_km + a
+print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
+
+
+
+
+
